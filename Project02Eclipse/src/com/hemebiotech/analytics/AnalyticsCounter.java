03403e6d82bc0aaa8883316514fe9b369775bd33
HichamSab <h.azzed78@hotmail.fr>
2021-01-12 17:09:16 +0100

    utilisation de la classe ReadSymptomDataFromFile

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -11,34 +11,27 @@ public class AnalyticsCounter {
 	
 	public static void main(String args[]) throws Exception {
 		// 1ere étape: on lit le ficher symptoms.txt READ
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-
-		ReadSymptomDataFromFile reader2 = new ReadSymptomDataFromFile("symptoms.txt");
-		List<String> allSymptoms = reader2.GetSymptoms();
+		ReadSymptomDataFromFile reader = new ReadSymptomDataFromFile("symptoms.txt");
+		List<String> allSymptoms = reader.GetSymptoms();
 		System.out.println(Arrays.toString(allSymptoms.toArray()));
-
-
 
 
 		// 2eme etapes: on parcours le fichier en comptant les symptoms COUNT (Map)
 		Map<String, Integer> symptomsCounter = new HashMap<>();
-		while (line!=null){
 
-			if (symptomsCounter.containsKey(line)){
+		for (String symptom: allSymptoms) {
+			if (symptomsCounter.containsKey(symptom)) {
 				//on ajoute +1 à la valeur de cette clé
-				symptomsCounter.put(line,symptomsCounter.get(line)+1);
-			}
-			else {
+				symptomsCounter.put(symptom, symptomsCounter.get(symptom) + 1);
+			} else {
 				// on crée ce nouveau symptome et on l'initialise cette valeur à 1
-				symptomsCounter.put(line,1);
+				symptomsCounter.put(symptom, 1);
 			}
-			line= reader.readLine();
 		}
 
 		// Afficher la map
 		for (Map.Entry entry : symptomsCounter.entrySet()) {
-			System.out.println("key: "+entry.getKey()+"; value: "+entry.getValue());
+			//System.out.println("key: "+entry.getKey()+"; value: "+entry.getValue());
 		}
 
 
@@ -46,11 +39,11 @@ public class AnalyticsCounter {
 		List<String> symptoms = new ArrayList<>(symptomsCounter.keySet());
 
 		// Afficher la list
-		System.out.println(Arrays.toString(symptoms.toArray()));
+		//System.out.println(Arrays.toString(symptoms.toArray()));
 		Collections.sort(symptoms);
-		System.out.println(Arrays.toString(symptoms.toArray()));
+		//System.out.println(Arrays.toString(symptoms.toArray()));
 		for (int i = 0; i < symptoms.size(); i++){
-			System.out.println(symptoms.get(i)+" = "+symptomsCounter.get(symptoms.get(i)));
+			//System.out.println(symptoms.get(i)+" = "+symptomsCounter.get(symptoms.get(i)));
 		}
 
 
--- Project02Eclipse/src/com/hemebiotech/analytics/count/CountSymptom.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/count/CountSymptom.java
@@ -1,4 +1,23 @@
 package com.hemebiotech.analytics.count;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CountSymptom {
+    public Map<String, Integer> count(List<String> symptoms){
+        Map<String, Integer> symptomsCounter = new HashMap<>();
+
+        for (String symptom: symptoms) {
+            if (symptomsCounter.containsKey(symptom)) {
+                //on ajoute +1 à la valeur de cette clé
+                symptomsCounter.put(symptom, symptomsCounter.get(symptom) + 1);
+            } else {
+                // on crée ce nouveau symptome et on l'initialise cette valeur à 1
+                symptomsCounter.put(symptom, 1);
+            }
+        }
+        return symptomsCounter;
+
+    }
 }
