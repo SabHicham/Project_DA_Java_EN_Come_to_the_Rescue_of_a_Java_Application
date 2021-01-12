910e8f907ecab4d0f34eb9b96b3edd0ff0bd76f3
HichamSab <h.azzed78@hotmail.fr>
2021-01-12 16:22:22 +0100

    création des package et des classes

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,85 @@
 package com.hemebiotech.analytics;
 
+import com.hemebiotech.analytics.read.ReadSymptomDataFromFile;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.*;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
 	
 	public static void main(String args[]) throws Exception {
-		// first get input
+		// 1ere étape: on lit le ficher symptoms.txt READ
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+		ReadSymptomDataFromFile reader2 = new ReadSymptomDataFromFile("symptoms.txt");
+		List<String> allSymptoms = reader2.GetSymptoms();
+		System.out.println(Arrays.toString(allSymptoms.toArray()));
 
-			line = reader.readLine();	// get another symptom
+
+
+
+		// 2eme etapes: on parcours le fichier en comptant les symptoms COUNT (Map)
+		Map<String, Integer> symptomsCounter = new HashMap<>();
+		while (line!=null){
+
+			if (symptomsCounter.containsKey(line)){
+				//on ajoute +1 à la valeur de cette clé
+				symptomsCounter.put(line,symptomsCounter.get(line)+1);
+			}
+			else {
+				// on crée ce nouveau symptome et on l'initialise cette valeur à 1
+				symptomsCounter.put(line,1);
+			}
+			line= reader.readLine();
 		}
-		
-		// next generate output
+
+		// Afficher la map
+		for (Map.Entry entry : symptomsCounter.entrySet()) {
+			System.out.println("key: "+entry.getKey()+"; value: "+entry.getValue());
+		}
+
+
+		// 3éme étapes: on range dans l'ordre alphabétique les symptomes SORT Collections.sort
+		List<String> symptoms = new ArrayList<>(symptomsCounter.keySet());
+
+		// Afficher la list
+		System.out.println(Arrays.toString(symptoms.toArray()));
+		Collections.sort(symptoms);
+		System.out.println(Arrays.toString(symptoms.toArray()));
+		for (int i = 0; i < symptoms.size(); i++){
+			System.out.println(symptoms.get(i)+" = "+symptomsCounter.get(symptoms.get(i)));
+		}
+
+
+
+
+
+
+
+		// 4éme étapes: on écrit le fichier result.out WRITE
 		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+		for (String symptom : symptoms) {
+			writer.write(symptom+" = "+symptomsCounter.get(symptom)+"\n");
+		}
 		writer.close();
+
+
+
+
+		// tester
+
+
+
+
+
+
+
+
+
+
+
 	}
 }
